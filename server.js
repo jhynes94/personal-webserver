f6d81ee50570f37e9edc76442275d5328b75c552
Justin Hynes-Bruell <jhynesbr@ltx-credence.com>
2017-11-09 10:52:25 -0500

    I think work is blocking the mongodb port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function () {
     console.log('listening on 3000')
@@ -15,11 +16,27 @@ app.get('/', (req, res) => {
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 })
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-  })
 
-  /*
-MongoClient.connect('link-to-mongodb', (err, database) => {
-    // ... start the server
-  })*/
+
+var db
+
+const url = 'mongodb://justinmongo:aFoolishPass4Mongo@ds251845.mlab.com:51845/test-db-personal-website';
+
+MongoClient.connect(url, (err, database) => {
+    if (err) return console.log(err)
+
+    db = database
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
+})
+
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('saved to database')
+        console.log(req.body)
+        res.redirect('/')
+    })
+})
