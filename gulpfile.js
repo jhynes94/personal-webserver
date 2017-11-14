938b7d468e945ddbebc4e4389b83d9e9d6756553
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2017-11-13 21:59:29 -0500

    Capstone

--- gulpfile.js
+++ gulpfile.js
@@ -16,4 +16,5 @@ gulp.task('dev', ['browserSync'], function() {
   // Reloads the browser whenever HTML or CSS files change
   gulp.watch('./public/css/*.css', browserSync.reload);
   gulp.watch('./public/*.html', browserSync.reload);
+  gulp.watch('./public/capstone/*.html', browserSync.reload);
 });
